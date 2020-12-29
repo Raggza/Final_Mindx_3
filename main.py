921816af99f85a4ea63c33345741682399bceedb
Raggza <ryomapro123@gmail.com>
2020-12-29 22:44:12 +0700

    hoan thien luong giac

--- main.py
+++ main.py
@@ -62,6 +62,28 @@ def sc(event):
         result = str(m.cos(float(no)))
     if text == 'tan':
         result = str(m.tan(float(no)))
+    if text == 'sinh':
+        result = str(m.sinh(float(no)))
+    if text == 'asin':
+        result = str(m.asin(float(no)))
+    if text == 'asinh':
+        result = str(m.asinh(float(no)))
+    if text == 'cos':
+        result = str(m.cos(float(no)))
+    if text == 'cosh':
+        result = str(m.cosh(float(no)))
+    if text == 'acos':
+        result = str(m.acos(float(no)))
+    if text == 'acosh':
+        result = str(m.acosh(float(no)))
+    if text == 'tan':
+        result = str(m.tan(float(no)))
+    if text == 'tanh':
+        result = str(m.tanh(float(no)))
+    if text == 'atan':
+        result = str(m.atan(float(no)))
+    if text == 'atanh':
+        result = str(m.atanh(float(no)))
     if text == 'lg':
         result = str(m.log10(float(no)))
     if text == 'ln':
@@ -87,6 +109,16 @@ def sc(event):
             result = str(m.pi)
         else:
             result = str(float(no) * m.pi)
+    if text == '2^':
+        if no == '':
+            result = "2**"
+        else:
+            result = str(2**float(no))
+    if text == '10^':
+        if no == '':
+            result = "10**"
+        else:
+            result = str(10**float(no))
 
     e.delete(0, END)
     e.insert(0, result)
@@ -127,6 +159,8 @@ nine = Button(TAB2, text="9", padx=29, pady=10, relief=RAISED, bg="Grey", fg="Wh
 div = Button(TAB2, text='/', padx=29, pady=10, relief=RAISED, bg="Black", fg="White", command=lambda: click("/"))
 
 tenpw = Button(TAB2, text='10^', padx=23, pady=10, relief=RAISED, bg="Black", fg='white', command=lambda: click("10**"))
+tenpw = Button(TAB2, text='10^', padx=23, pady=10, relief=RAISED, bg="Black", fg='white')
+tenpw.bind("<Button-1>", sc)
 four = Button(TAB2, text="4", padx=28, pady=10, relief=RAISED, bg="Grey", fg="White", command=lambda: click("4"))
 five = Button(TAB2, text="5", padx=29, pady=10, relief=RAISED, bg="Grey", fg="White", command=lambda: click("5"))
 six = Button(TAB2, text="6", padx=29, pady=10, relief=RAISED, bg="Grey", fg="White", command=lambda: click("6"))
@@ -257,5 +291,104 @@ equal.grid(row=6, column=2)
 plus.grid(row=6, column=3)
 '''------------------------------------------------------------------------------------------------------'''
 
+
+sinh = Button(TAB3, text="sinh", padx=29, pady=10, relief=RAISED, bg='black', fg='white')
+sinh.bind("<Button-1>", sc)
+asin = Button(TAB3, text="asin", padx=29, pady=10, relief=RAISED, bg='black', fg='white')
+asin.bind("<Button-1>", sc)
+acos = Button(TAB3, text='acos', padx=29, pady=10, relief=RAISED, bg='black', fg='white')
+acos.bind("<Button-1>", sc)
+atan = Button(TAB3, text='atan', padx=29, pady=10, relief=RAISED, bg='black', fg='white')
+atan.bind("<Button-1>", sc)
+ac = Button(TAB3, text='C', padx=34, pady=10, relief=RAISED, bg="#C82C0B", fg="White", command=lambda: clear())
+
+cosh = Button(TAB3, text='cosh', padx=27, pady=10, relief=RAISED, bg='black', fg='white')
+cosh.bind("<Button-1>", sc)
+asinh = Button(TAB3, text='asinh', padx=25, pady=10, relief=RAISED, bg='black', fg='white')
+asinh.bind("<Button-1>", sc)
+acosh = Button(TAB3, text='acosh', padx=25, pady=10, relief=RAISED, bg='black', fg='white')
+acosh.bind("<Button-1>", sc)
+atanh = Button(TAB3, text='atanh', padx=25, pady=10, relief=RAISED, bg='black', fg='white')
+atanh.bind("<Button-1>", sc)
+bksp = Button(TAB3, text='bksp', padx=25, pady=10, relief=RAISED, bg="#C82C0B", fg="White", command=lambda: bksps())
+
+tanh = Button(TAB3, text='tanh', padx=28, pady=10, relief=RAISED, bg='black', fg='white')
+tanh.bind("<Button-1>", sc)
+degb = Button(TAB3, text='deg', padx=29, pady=10, relief=RAISED, bg='black', fg='white')
+degb.bind("<Button-1>", sc)
+radb = Button(TAB3, text='radb', padx=29, pady=10, relief=RAISED, bg='black', fg='white')
+radb.bind("<Button-1>", sc)
+pib = Button(TAB3, text="pi", padx=34, pady=10, relief=RAISED, bg="Black", fg="White")
+pib.bind("<Button-1>", sc)
+div = Button(TAB3, text='/', padx=35, pady=10, relief=RAISED, bg="Black", fg="White", command=lambda: click("/"))
+
+sin = Button(TAB3, text='sin', padx=32, pady=10, relief=RAISED, bg='black', fg='white')
+sin.bind("<Button-1>", sc)
+seven = Button(TAB3, text="7", padx=36, pady=10, relief=RAISED, bg="Grey", fg="White", command=lambda: click("7"))
+eight = Button(TAB3, text="8", padx=36, pady=10, relief=RAISED, bg="Grey", fg="White", command=lambda: click("8"))
+nine = Button(TAB3, text="9", padx=36, pady=10, relief=RAISED, bg="Grey", fg="White", command=lambda: click("9"))
+mult = Button(TAB3, text="*", padx=36, pady=10, relief=RAISED, bg="Black", fg="White", command=lambda: click("*"))
+
+cos = Button(TAB3, text='cos', padx=31, pady=10, relief=RAISED, bg='black', fg='white')
+cos.bind("<Button-1>", sc)
+four = Button(TAB3, text="4", padx=36, pady=10, relief=RAISED, bg="Grey", fg="White", command=lambda: click("4"))
+five = Button(TAB3, text="5", padx=36, pady=10, relief=RAISED, bg="Grey", fg="White", command=lambda: click("5"))
+six = Button(TAB3, text="6", padx=36, pady=10, relief=RAISED, bg="Grey", fg="White", command=lambda: click("6"))
+minus = Button(TAB3, text="-", padx=36, pady=10, relief=RAISED, bg="Black", fg="White", command=lambda: click("-"))
+
+tan = Button(TAB3, text='tan', padx=32, pady=10, relief=RAISED, bg='black', fg='white')
+tan.bind("<Button-1>", sc)
+one = Button(TAB3, text="1", padx=36, pady=10, relief=RAISED, bg="Grey", fg="White", command=lambda: click("1"))
+two = Button(TAB3, text="2", padx=36, pady=10, relief=RAISED, bg="Grey", fg="White", command=lambda: click("2"))
+three = Button(TAB3, text="3", padx=36, pady=10, relief=RAISED, bg="Grey", fg="White", command=lambda: click("3"))
+plus = Button(TAB3, text="+", padx=35, pady=10, relief=RAISED, bg="Black", fg="White", command=lambda: click("+"))
+
+dot = Button(TAB3, text='.', padx=36, pady=10, relief=RAISED, bg="Black", fg="White", command=lambda: click("."))
+zero = Button(TAB3, text='0', padx=36, pady=10, relief=RAISED, bg="Grey", fg="White", command=lambda: click("0"))
+equal = Button(TAB3, text='=', padx=36, pady=10, relief=RAISED, bg="Orange", fg="Black", command=lambda: evaluate())
+
+sinh.grid(row=1, column=0)
+asin.grid(row=1, column=1)
+acos.grid(row=1, column=2)
+atan.grid(row=1, column=3)
+ac.grid(row=1, column=4)
+
+cosh.grid(row=2, column=0)
+asinh.grid(row=2, column=1)
+acosh.grid(row=2, column=2)
+atanh.grid(row=2, column=3)
+bksp.grid(row=2, column=4)
+
+tanh.grid(row=3, column=0)
+degb.grid(row=3, column=1)
+radb.grid(row=3, column=2)
+pib.grid(row=3, column=3)
+div.grid(row=3, column=4)
+
+sin.grid(row=4, column=0)
+seven.grid(row=4, column=1)
+eight.grid(row=4, column=2)
+nine.grid(row=4, column=3)
+mult.grid(row=4, column=4)
+
+cos.grid(row=5, column=0)
+four.grid(row=5, column=1)
+five.grid(row=5, column=2)
+six.grid(row=5, column=3)
+minus.grid(row=5, column=4)
+
+tan.grid(row=6, column=0)
+one.grid(row=6, column=1)
+two.grid(row=6, column=2)
+three.grid(row=6, column=3)
+plus.grid(row=6, column=4)
+
+dot.grid(row=7, column=1)
+zero.grid(row=7, column=2)
+equal.grid(row=7, column=3)
+
+
+TAB1.mainloop()
 TAB2.mainloop()
+TAB3.mainloop()
 window.mainloop()
